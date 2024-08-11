e73389bd68002e6f286c53613d9d5b57c7aa311b
razan <akangozan007@gmail.com>
2024-08-11 13:24:25 -0400

    Menambahkan Dashboard

--- login/index.php
+++ login/index.php
@@ -1,162 +1,105 @@
+<?php
+
+include '../config/koneksi.php';
+session_start();
+ 
+if (isset($_SESSION['username'])) {
+    header("Location: ../dashboard/index.php");
+    exit();
+}
+ 
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password']; // Hash the input password using SHA-256
+ 
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($koneksi, $sql);
+ 
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: ../dashboard/index.php");
+        exit();
+    } else {
+        echo "<script>alert('username atau password Anda salah. Silakan coba lagi!')</script>";
+    }
+}
+
+ 
+?>
 
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
-  <head>
-
+<head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>InterQuizz - Login</title>
     <!-- Favicons -->
     <link href="../assets/img/logointerquizz.png" rel="icon">
-
     <script src="https://owlcarousel2.github.io/OwlCarousel2/assets/vendors/jquery.min.js"></script>
-    
-  <!-- Fonts -->
-        <link href="https://fonts.googleapis.com" rel="preconnect">
-        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-        <!-- Vendor CSS Files -->
-        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
-        <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-     
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
+        /* Custom styles here */
     </style>
-
-  <!-- Main CSS File -->
-  <!-- <link href="assets/css/main.css" rel="stylesheet"> -->
-  <link href="../assets/vendor/bootstrap/css/login.css" rel="stylesheet">
-
+    <!-- Main CSS File -->
+    <link href="../assets/vendor/bootstrap/css/login.css" rel="stylesheet">
 </head>
-    
-
-  <body class="d-flex align-items-center py-4">
-    
-  <!-- header -->
-  <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center">
-
-      <a href="index.html" class="logo d-flex align-items-center me-auto">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">InterQuizz</h1>
-      </a>
-
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="#hero" class="active">Beranda</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
-
-
-    </div>
-  </header>
-  <!-- ./header -->
-   <!-- body -->
-  <main class="form-signin w-100 m-auto accent-background">
-    <!-- <main class="hero accent-background"> -->
-    <section class="">
-    <div class="container-fluid">
-      <!-- form login -->
-        <form method="post">
-          <img class="mb-4 mx-auto d-block" src="../assets/img/logointerquizz.png" alt="" width="112" height="97">
-          <!-- <h1 class="h3 mb-3 fw-normal">Please sign in</h1> -->
-
-          <div class="form-floating">
-            <input type="email" name="username" class="form-control text-subtle" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput" class="text-danger">Username</label>
-          </div>
-          <br>
-          <br>
-          <div class="form-floating">
-            <input type="password" name="password" class="form-control text-subtle" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword" class="text-danger">Password</label>
-          </div>
-
-          <div class="form-check text-start my-3">
-            <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Remember me
-            </label>
-          </div>
-          <button class="btn btn-primary w-100 py-2" name="submit" type="submit">Login</button>
-          <p class="mt-5 mb-3 text-body-light">Kamelia Khoirunnisa &copy; 2024-2025</p>
-        </form>
-      <!-- ./form login -->
-    </div>
-    </section>
-  </main>
-  <div id="preloader"></div>
-
-
-  
-  </body>
-  <!-- ./body -->
-
+<body class="d-flex align-items-center py-4">
+    <!-- header -->
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+            <a href="index.html" class="logo d-flex align-items-center me-auto">
+                <h1 class="sitename">InterQuizz</h1>
+            </a>
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="#hero" class="active">Beranda</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+        </div>
+    </header>
+    <!-- ./header -->
+    <!-- body -->
+    <main class="form-signin w-100 m-auto accent-background">
+        <section class="">
+            <div class="container-fluid">
+                <!-- form login -->
+                <form method="POST" action="">
+                    <img class="mb-4 mx-auto d-block" src="../assets/img/logointerquizz.png" alt="" width="112" height="97">
+                    <div class="form-floating">
+                        <input type="text" name="username" class="form-control text-subtle" id="floatingInput" placeholder="name@example.com" required>
+                        <label for="floatingInput" class="text-danger">Username</label>
+                    </div>
+                    <br>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control text-subtle" id="floatingPassword" placeholder="Password" required>
+                        <label for="floatingPassword" class="text-danger">Password</label>
+                    </div>
+                    <div class="form-check text-start my-3">
+                        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Remember me
+                        </label>
+                    </div>
+                    <button class="btn btn-primary w-100 py-2" name="submit" type="submit">Login</button>
+                    <p class="mt-5 mb-3 text-body-light">Kamelia Khoirunnisa &copy; 2024-2025</p>
+              
+                </form>
+                <!-- ./form login -->
+            </div>
+        </section>
+    </main>
+    <div id="preloader"></div>
     <!-- Vendor JS Files -->
     <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/vendor/php-email-form/validate.js"></script>
@@ -166,8 +109,7 @@
     <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="../assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
     <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-
     <!-- Main JS File -->
     <script src="../assets/js/main.js"></script>
+</body>
 </html>
