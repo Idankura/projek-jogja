b6d897c31c47bd23f8615717e9d41857bb564511
root <root@kangrazan.web>
2024-08-18 12:38:22 +0800

    Menambahkan Page Buat soal

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,9 +6,9 @@ $password = "root";
 
 $koneksi = mysqli_connect($servername, $username, $password, $database);
 
-// if (!$koneksi) {
-//     die('Gagal terkoneksi: ' . mysqli_connect_error());
-// } else {
-//     echo 'Berhasil terkoneksi';
-// }
+if (!$koneksi) {
+    // die('Gagal terkoneksi: ' . mysqli_connect_error());
+} else {
+    // echo 'Berhasil terkoneksi';
+}
 ?>
